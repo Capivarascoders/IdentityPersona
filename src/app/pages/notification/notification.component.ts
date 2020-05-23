7683fe949147753345d2c06ffa91861fe74c46e2
Danilo <danilo@4cadia.com>
2020-05-23 20:46:16 -0300

    Complete Project for presentation

--- src/app/pages/notification/notification.component.ts
+++ src/app/pages/notification/notification.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-notifcation',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.css']
 })
-export class NotificationComponent implements OnInit {
+export class NotificationComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  title = 'NotBootstrap';
+  closeResult: string;
+    constructor(private modalService: NgbModal) {}
+      open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
   }
-
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
